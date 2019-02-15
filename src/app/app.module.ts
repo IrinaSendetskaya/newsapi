a8fa38a0288c5159e7c673fbf191384941023931
IrinaSendetskaya <Irina_Siandetskaya@epam.com>
2019-02-15 16:46:08 +0300

    add pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NewsApiService } from './news-api.service';
+import { FormsModule } from '@angular/forms';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule,
-     MatIconModule, MatSidenavModule, MatListModule, MatFormFieldModule } from '@angular/material';
+     MatIconModule, MatSidenavModule, MatListModule, MatFormFieldModule, MatPaginatorModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PageComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +28,8 @@ import { AppComponent } from './app.component';
     MatSidenavModule,
     MatListModule,
     MatFormFieldModule,
+    MatPaginatorModule,
+    FormsModule,
   ],
   providers: [NewsApiService],
   bootstrap: [AppComponent]
